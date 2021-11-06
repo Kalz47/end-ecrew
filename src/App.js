1fdca6cf2b6fecd8ad44ce6b385aaeeb83b28fd9
Kalana <parkwaylabs@Parkways-MacBook-Air.local>
2021-11-06 13:38:46 +0530

    to checkout

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Client/Pages/Home/index";
 import Nav from "./Client/Components/Navbar";
 import About from "./Client/Pages/About/index";
-import ContactUs from "./Client/Pages/ContactUs";
+import ContactUs from "./Client/Pages/ContactUs/index";
+import Footer from "./Client/Components/Footer";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<ContactUs />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<ContactUs />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/Client/Components/Container.js
+++ src/Client/Components/Container.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Container({ children }) {
-  return <div className="mt-0">{children}</div>;
+  return <div className="mt-0 h-full">{children}</div>;
 }
