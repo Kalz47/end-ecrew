ac7093021c69c4c963d7d3c43e9321db172ac3c9
Chanaka97n <chanaka97n@gmail.com>
2021-11-06 23:51:27 +0530

    added cards details

--- src/Admin/Pages/AdminHome/index.js
+++ src/Admin/Pages/AdminHome/index.js
@@ -20,12 +20,12 @@ export default function AdminHome() {
         </button>
       </div>
       <div className={`${tab === 0 ? "block" : "hidden"}`}>
-        <form class="bg-white shadow rounded-lg p-6">
-          <div class="grid lg:grid-cols-2 gap-6">
-            <div class="border focus-within:border-blue-500 focus-within:text-blue-500 transition-all duration-500 relative rounded p-1">
-              <div class="-mt-4 absolute tracking-wider px-1 uppercase text-xs">
+        <form className="bg-white shadow rounded-lg p-6">
+          <div className="grid lg:grid-cols-2 gap-6">
+            <div className="border focus-within:border-blue-500 focus-within:text-blue-500 transition-all duration-500 relative rounded p-1">
+              <div className="-mt-4 absolute tracking-wider px-1 uppercase text-xs">
                 <p>
-                  <label for="name" class="bg-white text-gray-600 px-1">
+                  <label for="name" className="bg-white text-gray-600 px-1">
                     Salon name *
                   </label>
                 </p>
@@ -36,14 +36,14 @@ export default function AdminHome() {
                   autocomplete="false"
                   tabindex="0"
                   type="text"
-                  class="py-1 px-1 text-gray-900 outline-none block h-full w-full"
+                  className="py-1 px-1 text-gray-900 outline-none block h-full w-full"
                 />
               </p>
             </div>
-            <div class="border focus-within:border-blue-500 focus-within:text-blue-500 transition-all duration-500 relative rounded p-1">
-              <div class="-mt-4 absolute tracking-wider px-1 uppercase text-xs">
+            <div className="border focus-within:border-blue-500 focus-within:text-blue-500 transition-all duration-500 relative rounded p-1">
+              <div className="-mt-4 absolute tracking-wider px-1 uppercase text-xs">
                 <p>
-                  <label for="lastname" class="bg-white text-gray-600 px-1">
+                  <label for="lastname" className="bg-white text-gray-600 px-1">
                     Contact No *
                   </label>
                 </p>
@@ -54,14 +54,14 @@ export default function AdminHome() {
                   autocomplete="false"
                   tabindex="0"
                   type="text"
-                  class="py-1 px-1 outline-none block h-full w-full"
+                  className="py-1 px-1 outline-none block h-full w-full"
                 />
               </p>
             </div>
-            <div class="border focus-within:border-blue-500 focus-within:text-blue-500 transition-all duration-500 relative rounded p-1">
-              <div class="-mt-4 absolute tracking-wider px-1 uppercase text-xs">
+            <div className="border focus-within:border-blue-500 focus-within:text-blue-500 transition-all duration-500 relative rounded p-1">
+              <div className="-mt-4 absolute tracking-wider px-1 uppercase text-xs">
                 <p>
-                  <label for="username" class="bg-white text-gray-600 px-1">
+                  <label for="username" className="bg-white text-gray-600 px-1">
                     Grade *
                   </label>
                 </p>
@@ -72,22 +72,110 @@ export default function AdminHome() {
                   autocomplete="false"
                   tabindex="0"
                   type="text"
-                  class="py-1 px-1 outline-none block h-full w-full"
+                  className="py-1 px-1 outline-none block h-full w-full"
                 />
               </p>
             </div>
+            <div className="flex flex-row w-64">
+              <div>
+                <label for="lastname" className="bg-white text-gray-600 px-1">
+                  Open Time{" "}
+                </label>{" "}
+                <div>
+                  <div className="p-2 w-40 bg-white rounded-lg text-gray-600">
+                    <div className="flex">
+                      <select
+                        name="hours"
+                        className="bg-transparent text-xl appearance-none outline-none bg-white text-gray-600 rounded-lg"
+                      >
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                        <option value="10">10</option>
+                        <option value="11">10</option>
+                        <option value="12">12</option>
+                      </select>
+                      <span className="text-xl mr-3">:</span>
+                      <select
+                        name="minutes"
+                        className="bg-transparent text-xl appearance-none outline-none mr-4"
+                      >
+                        <option value="0">00</option>
+                        <option value="30">30</option>
+                      </select>
+                      <select
+                        name="ampm"
+                        className="bg-transparent text-xl appearance-none outline-none"
+                      >
+                        <option value="am">AM</option>
+                        <option value="pm">PM</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+              </div>
 
-            <div class="inline-block relative w-64">
-              <select class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+              <div>
+                <label for="lastname" className="bg-white text-gray-600 px-1">
+                  Close Time{" "}
+                </label>{" "}
+                <div>
+                  <div className="p-2 w-40 bg-white rounded-lg text-gray-600">
+                    <div className="flex">
+                      <select
+                        name="hours"
+                        className="bg-transparent text-xl appearance-none outline-none bg-white text-gray-600"
+                      >
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                        <option value="10">10</option>
+                        <option value="11">10</option>
+                        <option value="12">12</option>
+                      </select>
+                      <span className="text-xl mr-3">:</span>
+                      <select
+                        name="minutes"
+                        className="bg-transparent text-xl appearance-none outline-none mr-4"
+                      >
+                        <option value="0">00</option>
+                        <option value="30">30</option>
+                      </select>
+                      <select
+                        name="ampm"
+                        className="bg-transparent text-xl appearance-none outline-none"
+                      >
+                        <option value="am">AM</option>
+                        <option value="pm">PM</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="inline-block relative w-64">
+              <select className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
                 <option>
                   Really long option that will likely overlap the chevron
                 </option>
                 <option>Option 2</option>
                 <option>Option 3</option>
               </select>
-              <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                 <svg
-                  class="fill-current h-4 w-4"
+                  className="fill-current h-4 w-4"
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 20 20"
                 >
@@ -97,7 +185,7 @@ export default function AdminHome() {
             </div>
             <div>
               <label
-                class="
+                className="
                 w-64
                 flex flex-col
                 items-center
@@ -117,37 +205,39 @@ export default function AdminHome() {
                 duration-150
             "
               >
-                <i class="fa fa-cloud-upload-alt fa-3x"></i>
-                <span class="mt-2 text-base leading-normal">Select a file</span>
-                <input type="file" class="hidden" />
+                <i className="fa fa-cloud-upload-alt fa-3x"></i>
+                <span className="mt-2 text-base leading-normal">
+                  Select a file
+                </span>
+                <input type="file" className="hidden" />
               </label>
             </div>
             <div className="space-x-20">
-              <label class="inline-flex items-center mt-3">
+              <label className="inline-flex items-center mt-3">
                 <input
                   type="checkbox"
-                  class="form-checkbox h-5 w-5 text-gray-600"
+                  className="form-checkbox h-5 w-5 text-gray-600"
                 />
-                <span class="ml-2 text-gray-700">A/C</span>
+                <span className="ml-2 text-gray-700">A/C</span>
               </label>
-              <label class="inline-flex items-center mt-3">
+              <label className="inline-flex items-center mt-3">
                 <input
                   type="checkbox"
-                  class="form-checkbox h-5 w-5 text-gray-600"
+                  className="form-checkbox h-5 w-5 text-gray-600"
                 />
-                <span class="ml-2 text-gray-700">Parking</span>
+                <span className="ml-2 text-gray-700">Parking</span>
               </label>
-              <label class="inline-flex items-center mt-3">
+              <label className="inline-flex items-center mt-3">
                 <input
                   type="checkbox"
-                  class="form-checkbox h-5 w-5 text-gray-600"
+                  className="form-checkbox h-5 w-5 text-gray-600"
                 />
-                <span class="ml-2 text-gray-700">Wifi</span>
+                <span className="ml-2 text-gray-700">Wifi</span>
               </label>
             </div>
           </div>
-          <div class="border-t mt-6 pt-3">
-            <button class="rounded text-gray-100 px-3 py-1 bg-blue-500 hover:shadow-inner hover:bg-blue-700 transition-all duration-300">
+          <div className="border-t mt-6 pt-3">
+            <button className="rounded text-gray-100 px-3 py-1 bg-blue-500 hover:shadow-inner hover:bg-blue-700 transition-all duration-300">
               Save
             </button>
           </div>
@@ -155,10 +245,10 @@ export default function AdminHome() {
       </div>
       <div className={`p-10 ${tab === 1 ? "block" : "hidden"}`}>
         <form>
-          <div class="border focus-within:border-blue-500 focus-within:text-blue-500 transition-all duration-500 relative rounded p-1 w-40">
-            <div class="-mt-4 absolute tracking-wider px-1 uppercase text-xs">
+          <div className="border focus-within:border-blue-500 focus-within:text-blue-500 transition-all duration-500 relative rounded p-1 w-40">
+            <div className="-mt-4 absolute tracking-wider px-1 uppercase text-xs">
               <p>
-                <label for="name" class="bg-white text-gray-600 px-1">
+                <label for="name" className="bg-white text-gray-600 px-1">
                   Location *
                 </label>
               </p>
@@ -169,12 +259,12 @@ export default function AdminHome() {
                 autocomplete="false"
                 tabindex="0"
                 type="text"
-                class="py-1 px-1 text-gray-900 outline-none block h-full w-full"
+                className="py-1 px-1 text-gray-900 outline-none block h-full w-full"
               />
             </p>
           </div>
-          <div class="border-t mt-6 pt-3">
-            <button class="rounded text-gray-100 px-3 py-1 bg-blue-500 hover:shadow-inner hover:bg-blue-700 transition-all duration-300">
+          <div className="border-t mt-6 pt-3">
+            <button className="rounded text-gray-100 px-3 py-1 bg-blue-500 hover:shadow-inner hover:bg-blue-700 transition-all duration-300">
               Save
             </button>
           </div>{" "}
